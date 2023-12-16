f47de4335872ecb6c9593aa2360ae01dc8e93e43
Patrick3453 <133368906+Patrick3453@users.noreply.github.com>
2023-12-16 18:48:17 -0300

    Add files via upload

---
+++ codigo/cadastro.php
@@ -0,0 +1,34 @@
+<?php
+$dbHost = 'Localhost';
+$dbUsername = 'root';
+$dbPasswaord = '';
+$dbName = 'cadastro';
+
+
+$conexao = new mysqli($dbHost, $dbUsername, $dbPasswaord, $dbName);
+
+//if($conexao->connect_errno)
+//{
+  //  echo "Erro";
+
+//}
+//else
+//{
+  //  echo "Conexão efeutada com sucesso";
+
+//}
+
+
+//if(isset($_POST['submit']))
+//{
+  // print_r($_POST['nome']);
+ //  print_r($_POST['phone']);
+  
+   
+ //  print_r($_POST['data']);
+
+  
+//}
+
+ 
+?>
